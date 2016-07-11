bbdc0ac860c05096ce2d7dd9e31d389e8532cdc1
Sergio Navarro <jersio@hotmail.com>
2016-07-11 22:55:47 +0200

    Upgraded dependencies and removed controller for sign in users

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,59 +1,70 @@
-﻿using Morphous.TokenAuth.TransferModels;
-using Orchard.Localization;
-using System;
-using System.Web.Http;
-using Orchard.Users.Services;
-using Orchard.Users.Models;
-using Orchard.Security;
+﻿//using Morphous.TokenAuth.TransferModels;
+//using Orchard.Localization;
+//using System;
+//using System.Web.Http;
+//using Orchard.Users.Services;
+//using Orchard.Users.Models;
+//using Orchard.Security;
+//using Orchard;
+//using Orchard.ContentManagement;
 
-namespace Morphous.TokenAuth.Controllers {
-    [Authorize]
-    public class AccountController : ApiController {
-        private readonly IMembershipService _membershipService;
-        private readonly IUserService _userService;
+//namespace Morphous.TokenAuth.Controllers {
+//    [Authorize]
+//    public class AccountController : ApiController {
+//        private readonly IMembershipService _membershipService;
+//        private readonly IUserService _userService;
+//        private readonly IOrchardServices _orchardServices;
 
-        public AccountController(
-            IMembershipService membershipService,
-            IUserService userService) {
-            _membershipService = membershipService;
-            _userService = userService;
-            T = NullLocalizer.Instance;
-        }
+//        public AccountController(
+//            IMembershipService membershipService,
+//            IUserService userService,
+//            IOrchardServices orchardServices) {
+//            _membershipService = membershipService;
+//            _userService = userService;
+//            _orchardServices = orchardServices;
+//            T = NullLocalizer.Instance;
+//        }
 
-        public Localizer T { get; set; }
+//        public Localizer T { get; set; }
 
-        public string Get() {
-            return "test";
-        }
+//        public string Get() {
+//            return "test";
+//        }
 
-        [AllowAnonymous]
-        [HttpPost]
-        public IHttpActionResult Register(RegisterBindingModel model) {
-            if (model == null) {
-                return BadRequest();
-            }
+//        [AllowAnonymous]
+//        [HttpPost]
+//        public IHttpActionResult Register(RegisterBindingModel model) {
 
-            if (!ModelState.IsValid) {
-                return BadRequest(ModelState);
-            }
+//            var registrationSettings = _orchardServices.WorkContext.CurrentSite.As<RegistrationSettingsPart>();
+//            if (!registrationSettings.UsersCanRegister) {
+//                return NotFound();
+//            }
 
-            if (!string.IsNullOrEmpty(model.Email)) {
-                if (!_userService.VerifyUserUnicity(model.Email, model.Email)) {
-                    AddModelError("NotUniqueUserName", T("User with that email already exists."));
-                    return BadRequest(ModelState);
-                }
-            }
+//            if (model == null) {
+//                return BadRequest();
+//            }
 
-            var user = _membershipService.CreateUser(new CreateUserParams(model.Email, model.Password, model.Email, null, null, true));
-            if (user != null) {
-                return Ok();
-            }
+//            if (!ModelState.IsValid) {
+//                return BadRequest(ModelState);
+//            }
 
-            return InternalServerError();
-        }
+//            if (!string.IsNullOrEmpty(model.Email)) {
+//                if (!_userService.VerifyUserUnicity(model.Email, model.Email)) {
+//                    AddModelError("NotUniqueUserName", T("User with that email already exists."));
+//                    return BadRequest(ModelState);
+//                }
+//            }
 
-        public void AddModelError(string key, LocalizedString errorMessage) {
-            ModelState.AddModelError(key, errorMessage.ToString());
-        }
-    }
-}
+//            var user = _membershipService.CreateUser(new CreateUserParams(model.Email, model.Password, model.Email, null, null, true));
+//            if (user != null) {
+//                return Ok();
+//            }
+
+//            return InternalServerError();
+//        }
+
+//        public void AddModelError(string key, LocalizedString errorMessage) {
+//            ModelState.AddModelError(key, errorMessage.ToString());
+//        }
+//    }
+//}
